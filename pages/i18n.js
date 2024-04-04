fb7aa769485fa9ccdcc4b5a5616aeb6918661d09
narminmammadova1 <nerminytb89@gmail.com>
2024-04-04 15:08:52 +0400

    i18n fixed

--- pages/i18n.js
+++ pages/i18n.js
@@ -1,49 +1,44 @@
-// import i18n from "i18next";
-// import { initReactI18next } from "react-i18next";
-//  import enTranslation from "../public/locales/en/translation.json";
-// import frTranslation from "../public/locales/fr/translation.json";
-//  import azTranslation from "../public/locales/az/translation.json";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslation from "../public/locales/en/translation.json";
+import frTranslation from "../public/locales/fr/translation.json";
+import azTranslation from "../public/locales/az/translation.json";
 
-// const resources={
+const resources = {
+  en: {
+    translation: enTranslation
+  },
+  az: {
+    translation: azTranslation
+  },
+  fr: {
+    translation: frTranslation
+  }
+};
 
-// en:{
-//     translation:enTranslation
-// },
-// az:{
-//     translation:azTranslation
-// },
-// fr:{
-//     translation:frTranslation
+i18n
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    resources
+  });
+
+export { i18n };
+
+
+
+
+// import React from 'react';
+// import { useTranslation } from 'react-i18next';
+// function MyComponent() {
+//   const { t} = useTranslation();
+
+//   return (
+//     <div>
+//       <h1>{t('welcome')}</h1>
+//       <p>{t('description')}</p>
+//     </div>
+//   );
 // }
 
-// }
-
-// i18n
-// .use(initReactI18next)
-// .init({
-// lng:"en",
-// resources
-
-// })
-
-// export default i18n
-
-
-
-
-
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-
-function MyComponent() {
-  const { t } = useTranslation();
-
-  return (
-    <div>
-      <h1>{t('welcome')}</h1>
-      <p>{t('description')}</p>
-    </div>
-  );
-}
-
-export default MyComponent;
+// export default MyComponent;
